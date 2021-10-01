413026692e7d10b559fa3e9f9a4849dfca9f2e67
varunmohta <mohtavarun70@gmail.com>
2021-10-01 14:15:58 +0530

    Update home.php

--- website/content/home.php
+++ website/content/home.php
@@ -2,4 +2,4 @@
 Feel free to edit it and check in git to test the CI/CD flow</p>
 </p>It is a long established fact that no one reads this and I may add anything and reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
 <p> yes hello from varun </p>
-<p> demo started !!!!</p>
+<p> demo started !!!!!!!!</p>
